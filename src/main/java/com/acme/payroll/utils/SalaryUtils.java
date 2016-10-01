61b44dfa4df6d663db279dee2830b5edd9eebb35
fmrsabino <fredx96@gmail.com>
2016-10-01 13:16:20 +0100

    Salary digits are now grouped

--- src/main/java/com/acme/payroll/utils/SalaryUtils.java
+++ src/main/java/com/acme/payroll/utils/SalaryUtils.java
@@ -4,12 +4,16 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class SalaryUtils {
+    public static final int GROUP_SIZE = 3;
+
     public static double convertSalary(double amount, double rate) {
         return rate * amount;
     }
 
     public static String formatSalary(double amount) {
         DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        decimalFormat.setGroupingUsed(true);
+        decimalFormat.setGroupingSize(GROUP_SIZE);
         decimalFormat.setRoundingMode(RoundingMode.HALF_UP);
         return decimalFormat.format(amount);
     }
